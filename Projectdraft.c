08a19ed3a90c85d68369a1b30c4417715361b607
Mikaiyl i3nigma <ssmdm56@gmail.com>
2015-04-23 18:18:30 -0400

    With inputs

--- Projectdraft.c
+++ Projectdraft.c
@@ -2,8 +2,6 @@
 #include <mc9s12dp256.h>     /* registers & ports definitions */
 #pragma LINK_INFO DERIVATIVE "mc9s12dp256b"
 #include <stdio.h>
-#define ENABLE 0b00000010//ENable line of LCDC is bit 1 of PORTK
-#define DATASEL 0b00000001 //DATASEL will set reg select (bit0 of PORTK) to 1
 #define pitch ATD0DR3H
 #define volume ATD0DR4H
 
@@ -16,38 +14,42 @@ unsigned char seg7[] ={0x3f, 0x06,0x5b,0x4f,0x66,
 
 
 interrupt void RTI_ISR(void) 
-{   absoluteTime++; // uses global vaariable
-    
-    CRGFLG = 0x80; /* clear RTIF bit to enable next interrupt*/
-    
-    if ((absoluteTime % (ms/50)) == 0) {
-        
-	  PTT^= 0b00100000;// toggle  PTT to crate halp an audio wave on speaker.
-} 
-     if(absoluteTime % 4 == 0){
-       ms++;
-       if(ms == 999){
-          sec++;
-          ms=0;
-          if(sec == 59){
-            min++;
-            sec=0;
-            if(min == 59){
-              hr++;
-              min=0;
-            }
+  {   
+      CRGFLG = 0x80; /* clear RTIF bit to enable next interrupt*/
+      absoluteTime++; // uses global vaariable
+      if(PTH & 0x01){
+      
+      if(PTH & 0x01){
+          PWMDTY4 = ((PWMPER4/2)/(25500/(volume * 100)));
+      }else{
+          PWMDTY4 = 0;
           }
-       }
-    }
-
-}
+  
+      if ((absoluteTime % (ms/50)) == 0) {
+  	    PTT^= 0x20;// toggle  PTT to crate halp an audio wave on speaker.
+      } 
+      if(absoluteTime % 4 == 0){
+        ms++;
+         if(ms == 999){
+            sec++;
+            ms=0;
+            if(sec == 59){
+              min++;
+              sec=0;
+                if(min == 59){
+                  hr++;
+                  min=0;}
+               }
+            }
+         }
+      }
+  }
 
 interrupt void UnimplementedISR(void) 
 {  
 
    bogusISRFlag=1; // handle bogus interrupts
 }
-
 
 typedef void (*near tIsrFunc)(void); // tIsrFunc is a pointer to a function
 
@@ -64,21 +66,6 @@ const tIsrFunc _vect[] @0x3E70 =
         UnimplementedISR,    /* vector 01: Clock monitor fail */
         UnimplementedISR		 /* Reset vector */
  };
-
-void RTIInit(unsigned char rtictlvalue) {
-  /* setup RTI interrupt frequency & enable RTI interrupts, assuming 4MHz xtal */
-  /* clock divider set in RTICTL reg, and is (N+1)*2^(M+9), where N=bits(3:0), M=bits(6:4).
-    But M=0 disables divider.
-    f= 4MHz / ((n+1)*2^(M+9)).  
-    Useful values:  0x10 = 4kHz, 0x11 = 2kHz, 0x12 = 1.33kHz, 0x13 = 1kHz
-   */  
-   if (!rtictlvalue) //if zero, set to default
-		RTICTL =0x10;
-	else
-		RTICTL = rtictlvalue; /*set RTI prescaler */ 
-    CRGINT = 0b10000000; /* enable RTI interrupts */ 
-}
-  
      
 void Welcome (void){
      
@@ -87,21 +74,17 @@ void Welcome (void){
      delayms(3000);
 }
 
-void main(void) 
- {
- 
-  char buffer[32]; //convert binary value to character string written to buffer
-  
+void init(){
   LCDinit();
+  ATDinit();
+  RTIInit(0x11);
   
   DDRB = 0xFF;    //PORTB as output
   DDRT = 0xff;
   DDRJ = 0xFF;    //PTJ as output for Dragon12+ LEDs
   PTJ=0x0;        //display data on PORTB pins
   PTP = 0xFF;
-  
-  
-  
+     
   PWMPRCLK = 0x03;   // 4MHz / 8 = 500kHz
   PWMSCLA = 0x0A;    // 500kHz / (2*10) =25kHz
   PWMCLK = 0x20;     // PWM source channel 5
@@ -111,19 +94,24 @@ void main(void)
   PWME = 0x20;       // Enable PWM5
   
   ms=0, sec=0, min=0, hr=0;
-  
-  RTIInit(0x11);
-  
+   
   EnableInterrupts;
-  
   Welcome();
-  
-  for(;;)
+
+};
+
+void main(void) 
+ {
+ 
+  char buffer[32]; //convert binary value to character string written to buffer
+  init();
+ 
+  while(1)
   {
     
 
 		
-	sprintf(buffer, "Frequency: %d \n L1: %d L2: %d%d \n", hr, min, sec, ms);
+	(void)sprintf(buffer, "Frequency: %d \nL1: %d L2: %d ", volume, volume, pitch);
 	
 	LCDstring(buffer); //print the buffer string
 
